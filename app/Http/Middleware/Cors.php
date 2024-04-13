fe23bab743c3c4950c8e37080e0b2a73e05d0794
Alexander Sánchez <alexander.sanchez@idoagency.com>
2024-04-13 17:42:44 -0600

    fix cors 2

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,6 +15,12 @@ class Cors
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+
+        return $response;
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-            //\App\Http\Middleware\Cors::class,
-            \Illuminate\Http\Middleware\HandleCors::class,
+
+        ]);
+        $middleware->api(append: [
+            \App\Http\Middleware\Cors::class,
         ]);
 
         //
